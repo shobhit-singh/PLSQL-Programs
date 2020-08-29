af75760171b989dc723f371dcbe5f668b2903ae8
Shobhit <shobhitsingh2007@gmail.com>
2020-08-29 22:31:23 +0530

    Update varray_01.sql

--- varray_01.sql
+++ varray_01.sql
@@ -1,6 +1,13 @@
 SET SERVEROUTPUT ON;
 
 DECLARE
+/*
++ Array in other languages - VARRAY in PL/SQL: Collection of items of same datatype & has a maximum size. 
++ When defining a VARRAY type, you must specify its maximum size. So fixed upper bound. Subscript is integer (i.e. index) starts from 1
++ VARRAY is always dense (consecutive subscript). You cannot delete an item in middle, but we can trim elements from end. 
++ VARRAY is preferred when no. of elements known & accessed using sequence i.e. index.
+*/
+
     TYPE va_char_type IS
         VARRAY(4) OF VARCHAR(15);
     v1  va_char_type := va_char_type('East', 'West', 'North', 'South');  -- initialize varray in declaration using constructor
